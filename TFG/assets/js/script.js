0b68585c216f059d5299d7b22f433960654cac4f
Marcos Valero <133355184+marc0sV@users.noreply.github.com>
2024-05-30 19:39:16 +0200

    Add files via upload

---
+++ TFG/assets/js/script.js
@@ -0,0 +1,27 @@
+function redirect_index(){
+    window.location.href = "../php/index.php";
+}
+
+function redirect_sesion(){
+    window.location.href = "../php/inicioSesion.php";
+}
+
+function redirect_info(){
+    window.location.href = "../php/info.php";
+}
+
+function redirect_crearEquipo(){
+    window.location.href = "../php/crearEquipo.php";
+}
+
+function redirect_Pokedex(){
+    window.location.href = "../php/Pokedex.php";
+}
+
+function redirect_tablaTipos(){
+    window.location.href = "../php/tablaTipos.php";
+}
+
+function redirect_register(){
+    window.location.href = "../php/registro.php";
+}

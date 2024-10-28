15ded4cf6f9cfd2c83aed99a54c72f6b1ede1d48
Marcos Valero <133355184+marc0sV@users.noreply.github.com>
2024-10-28 19:26:20 +0100

    Update config.php

--- Code/php/config.php
+++ Code/php/config.php
@@ -1,6 +1,6 @@
 <?php
     const BBDD_NAME = "tfg"; 
-    const BBDD_PASSWORD = "Espantotoledo1";
+    const BBDD_PASSWORD = "****";
     const BBDD_HOST = "localhost";
     const BBDD_USER = "marcos";
 ?>

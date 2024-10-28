9fc5e28db937bd146d57b6362689fb36c09a520a
Marcos Valero <133355184+marc0sV@users.noreply.github.com>
2024-10-28 19:20:50 +0100

    Delete TFG directory

--- TFG/php/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p class="footer-p">Pokemon is © of Nintendo 1995- <?php echo date("Y"); ?></p>
-    <p class="footer-p">© of Marcos Valero</p>
-    <p class="footer-p"> Contacto: 
-        <a target="_blank" href="mailto:marcosvaleromoral@gmail.com" class="footer-link">marcosvaleromoral@gmail.com</a></p>
-</footer>
